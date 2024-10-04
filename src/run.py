f67153060ac489dd746c23752a6768ea9fd07921
Sajad Rahmanian <ra.sajad79@gmail.com>
2024-10-04 20:41:08 +0000

    Part g

--- src/run.py
+++ src/run.py
@@ -162,7 +162,7 @@ elif args.function == 'finetune':
         lr_decay=True,
         warmup_tokens=512 * 20,
         final_tokens=200 * len(pretrain_dataset) * block_size,
-        num_workers=0,
+        num_workers=4,
         writer=writer,
         ckpt_path=args.writing_params_path
     )
